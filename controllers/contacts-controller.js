184a0dbfe63098cc5afe5018cf8b56796307dc53
MHatalyak <matronahu@gmail.com>
2023-10-15 17:19:55 +0300

    added files

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -4,13 +4,15 @@ import { HttpError } from "../helpers/index.js";
 import { controlWrapper } from "../decorators/index.js";
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find({});
+  const { _id: owner } = req.user;
+  const result = await Contact.find({ owner }).populate("owner", "email");
   res.status(200).json(result);
 };
 
 const getContactById = async (req, res) => {
+  const { _id: owner } = req.user;
   const { contactId } = req.params;
-  const result = await Contact.findById(contactId);
+  const result = await Contact.findOne({ _id: contactId, owner });
   if (!result) {
     throw HttpError(404, `Not found`);
   }
@@ -18,14 +20,19 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 
 const updateContactById = async (req, res) => {
   const { contactId } = req.params;
+  const { _id: owner } = req.user;
 
-  const result = await Contact.findByIdAndUpdate(contactId, req.body);
+  const result = await Contact.findOneAndUpdate(
+    { _id: contactId, owner },
+    req.body
+  );
   if (!result) {
     throw HttpError(404, `Not found`);
   }
@@ -35,8 +42,12 @@ const updateContactById = async (req, res) => {
 
 const updateStatusContact = async (req, res) => {
   const { contactId } = req.params;
+  const { _id: owner } = req.user;
 
-  const result = await Contact.findByIdAndUpdate(contactId, req.body);
+  const result = await Contact.findOneAndUpdate(
+    { _id: contactId, owner },
+    req.body
+  );
   if (!result) {
     throw HttpError(404, `Not found`);
   }
@@ -46,7 +57,9 @@ const updateStatusContact = async (req, res) => {
 
 const removeContact = async (req, res) => {
   const { contactId } = req.params;
-  const result = await Contact.findByIdAndDelete(contactId);
+  const { _id: owner } = req.user;
+
+  const result = await Contact.findOneAndDelete({ _id: contactId });
   if (!result) {
     throw HttpError(404, `Not found`);
   }
---
+++ models/user.js
@@ -0,0 +1,52 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+
+import { handleSaveError, runValidatorsAtUpdate } from "./hooks.js";
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      match: emailRegexp,
+      unique: true,
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: [true, "Set password for user"],
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+    },
+  },
+  { versionKey: false }
+);
+
+userSchema.post("save", handleSaveError);
+
+userSchema.pre("findOneAndUpdate", runValidatorsAtUpdate);
+
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+export const userRegistSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+  subscription: Joi.string(),
+});
+
+export const userLoginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const User = model("user", userSchema);
+
+export default User;
